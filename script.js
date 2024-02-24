06a97d35cf3298b1fba0bedb6f562559f0f7c3c7
aymen ghemam <aymen.ghemam_hamed@smeetz.com>
2024-02-24 21:47:10 +0100

    update links

---
+++ script.js
@@ -0,0 +1,30 @@
+const links = document.querySelectorAll('a');
+let focusedIndex = 7;
+
+links[focusedIndex].focus();
+
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'ArrowDown' || event.key === 'ArrowRight') {
+    focusedIndex = (focusedIndex + 1) % links.length;
+    updateFocus();
+    event.preventDefault();
+  } else if (event.key === 'ArrowUp' || event.key === 'ArrowLeft') {
+    focusedIndex = (focusedIndex - 1 + links.length) % links.length;
+    updateFocus();
+    event.preventDefault();
+  } else if (event.key === 'Enter') {
+    links[focusedIndex].click();
+  }
+});
+
+function updateFocus() {
+  links.forEach((link, index) => {
+    if (index === focusedIndex) {
+      link.focus();
+    //   link.classList.add('active');
+    } else {
+    //   link.classList.remove('active');
+    link.blur();
+    }
+  });
+}
